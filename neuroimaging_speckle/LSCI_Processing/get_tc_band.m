09d0718f2843d386f966a35c057856195f449704
Haad <haad.rathore@gmail.com>
2022-09-19 16:19:05 -0500

    Update to Camera magnification and scaling
    The optical system was updated and the new values for scaling and magnification are changed

--- neuroimaging_speckle/LSCI_Processing/get_tc_band.m
+++ neuroimaging_speckle/LSCI_Processing/get_tc_band.m
@@ -2,7 +2,7 @@ function t=get_tc_band(K,exp_time,beta)
   tc_tmp=logspace(-10,-1,50001); 
   K_tmp=sqrt(beta.*tc_tmp.*tc_tmp./(2*exp_time.*exp_time).*(exp(-2*exp_time./tc_tmp)-1+(2*exp_time./tc_tmp)));
   
-  if(ndims(K)==2 & min(size(K))==1)
+  if(ndims(K)== 2 && min(size(K)) == 1)
       t=interp1(K_tmp,tc_tmp,K);
   else
       Kfoo=reshape(K,[1 prod(size(K))]);
