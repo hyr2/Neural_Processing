9ae1d5764b3b94c149c5bfcc4697e845c23d34c7
Haad <haad.rathore@gmail.com>
2022-09-28 17:47:09 -0500

    Functionality for manually adding mask for craniotomy

--- neuroimaging_speckle/LSCI_Processing/longitudinal_rICT_fullframe_MESI.m
+++ neuroimaging_speckle/LSCI_Processing/longitudinal_rICT_fullframe_MESI.m
@@ -15,6 +15,8 @@ Nseq = 200;
 NFrames = Nexp * Nseq; 
 idx = 95*Nexp+1:105*Nexp;      % middle 10 seq
 iter_num = 1;
+filename_local_sc_bl = {};
+filename_local_sc_st = {};
 % Main processing for loop
 for iter_local = folder_datasets_mouse
     folder_local = fullfile(input_folder,iter_local);
@@ -32,7 +34,9 @@ for iter_local = folder_datasets_mouse
     sc_bl = reshape(sc_bl,size(sc_bl,1),size(sc_bl,2),Nexp,[]);
     sc_bl = mean(sc_bl,4);
     filename_local_sc_bl{iter_num} = fullfile(folder_local,[folder_datasets{1},'.sc']);
-%     write_sc(sc_bl, filename_local_sc_bl{iter_num});
+    mask_bl = defineCraniotomy(sc_bl(:,:,9),[0.03,0.4]);
+    sc_bl = sc_bl .* mask_bl;
+    write_sc(sc_bl, filename_local_sc_bl{iter_num});
     % Post-Stroke
     folder_datasets_st = fullfile(folder_local,folder_datasets{2});
     folder_datasets_st = dir_sorted(fullfile(folder_datasets_st,'*.sc'));
@@ -40,7 +44,9 @@ for iter_local = folder_datasets_mouse
     sc_st = reshape(sc_st,size(sc_st,1),size(sc_st,2),Nexp,[]);
     sc_st = mean(sc_st,4);
     filename_local_sc_st{iter_num} = fullfile(folder_local,[folder_datasets{2},'.sc']);
-%     write_sc(sc_bl, filename_local_sc_st{iter_num});
+    mask_st = defineCraniotomy(sc_st(:,:,9),[0.03,0.4]);
+    sc_st = sc_st .* mask_st;
+    write_sc(sc_st, filename_local_sc_st{iter_num});
     iter_num = iter_num + 1;
 end
 % Prepare cell array for parallel proc
@@ -56,8 +62,8 @@ idx = strfind(code_dir,'/');
 code_dir = code_dir(1:idx(end)-1);
 c_compiled_mesi = fullfile(code_dir,'mesi.linux');
 parpool;
-parfor i = 1:size(filename_local_sc_bl,2)
-    CMD = sprintf('%s %s -n 1',c_compiled_mesi,filename_local_sc_bl{1,i});
+parfor i = 1:size(cl,2)
+    CMD = sprintf('%s %s -n 1',c_compiled_mesi,cl{1,i});
     system(CMD);
 end
 delete(gcp('nocreate'));
