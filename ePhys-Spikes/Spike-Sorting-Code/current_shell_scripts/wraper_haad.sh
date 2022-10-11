f221f2e741f1f397a05bc80b2ef1428e87c8a4e9
Haad <haad.rathore@gmail.com>
2022-10-10 19:07:14 -0500

    minor changes to folder management

--- ePhys-Spikes/Spike-Sorting-Code/current_shell_scripts/wraper_haad.sh
+++ ePhys-Spikes/Spike-Sorting-Code/current_shell_scripts/wraper_haad.sh
@@ -1,11 +1,13 @@
 input_dirs=(\
-"/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sort/spikesort_out/Kai/2021-12-17" \
-# "/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sort/spikesort_out/2021-12-17" \
+"/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sort/spikesort_out/Haad/RH3/10-3/ePhys/data_221003_111727" \
+"/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sort/spikesort_out/Haad/RH3/10-4/ePhys/data_221004_100418" \
+"/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sort/spikesort_out/Haad/RH3/10-4-b/ePhys/data_221004_145257" \
+"/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sort/spikesort_out/Haad/RH3/10-7/ePhys/data_221007_123731"
   )
-ouput_dirs=(\
-"/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sort/spikesort_out/Kai/2021-12-17" \
-# "/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sort/spikesort_out/2021-12-17" \
-  )
+# ouput_dirs=(\
+# "/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sort/spikesort_out/Jaanita/2021-12-17" \
+# # "/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sort/spikesort_out/2021-12-17" \
+#   )
 filename_json="/pre_MS.json"
 num_features_var=8
 max_num_clips_for_pca_var=1000
@@ -14,10 +16,10 @@ export ML_TEMPORARY_DIRECTORY=/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sor
 ovr_start_stamp=$SECONDS
 for i in "${!input_dirs[@]}"; do
   input_dir="${input_dirs[i]}"
-  ouput_dir="${ouput_dirs[i]}"
+  ouput_dir="${input_dirs[i]}"
   temp_path=$input_dir$filename_json
   samplerate=$(cat $temp_path | jq .SampleRate)
-  geom_file="${ouput_dirs[i]}/geom.csv"
+  geom_file="${input_dirs[i]}/geom.csv"
   num_features="$num_features_var"
   max_num_clips_for_pca="$max_num_clips_for_pca_var"
   echo ---------------------------------------
