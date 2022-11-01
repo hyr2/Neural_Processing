4b3fdde427022e2cd97207bf805d12e4975136d6
Haad <haad.rathore@gmail.com>
2022-11-01 18:51:12 -0500

    Updated code for reading .txt file output from LabView program

--- ePhys-Spikes/Spike-Sorting-Code/post_msort_processing/utils/read_stimtxt.py
+++ ePhys-Spikes/Spike-Sorting-Code/post_msort_processing/utils/read_stimtxt.py
@@ -2,16 +2,18 @@ def read_stimtxt(matlabTXT):
     # Read .txt file
     Txt_File = open(matlabTXT,"r")
     str1 = Txt_File.readlines()
-    stim_start_time = float(str1[3])        # Stimulation start time
+    stim_start_time = float(str1[3]) - 0.05       # Stimulation start time (50 ms error in labview)
     stim_num = int(str1[6])                 # Number of stimulations
     seq_period = float(str1[4])             # Period of each sequence consisting of 10 to 15 frames
-    # len_trials = int(str1[1]) + 2           # Length of a single trial in # seq
+    len_trials = int(str1[1])               # Length of a single trial in # seq
     num_trials = int(str1[7])               # total number of trials
-    # FramePerSeq = int(str1[2])              # frames per sequence. Important info to read the data.timing file
-    # total_seq = num_trials * len_trials
-    # len_trials_arr = list(range(1,total_seq,len_trials))
-    n_seq_per_trial = int(str1[1])
-    stim_duration = stim_num * seq_period
-    trial_duration = n_seq_per_trial * seq_period
+    FramePerSeq = int(str1[2])              # frames per sequence. Important info to read the data.timing file
+    total_seq = num_trials * len_trials
+    len_trials_arr = list(range(1,total_seq,len_trials))
+    print('Each sequence lasts...',seq_period,'sec')
+    print('Number of sequences in each trial...',len_trials)
+    print('Total number of trials in this dataset are:', num_trials)
+    print('Reading the total number of sequences in this dataset...',total_seq,'\n.\n.\n.')
     Txt_File.close()
-    return trial_duration, num_trials, stim_start_time, stim_duration
+    
+    return stim_start_time, stim_num, seq_period, len_trials, num_trials, FramePerSeq, total_seq, len_trials_arr
