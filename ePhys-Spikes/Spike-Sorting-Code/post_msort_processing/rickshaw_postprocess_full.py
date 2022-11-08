0e887bad690b2e50bbe8a592f20fd31fc19fdcc1
Haad <haad.rathore@gmail.com>
2022-11-08 10:50:44 -0600

    Added batch processing for population_analysis.py script
    Script is used to compute excitatory vs inhibitory masks and separates firing rates of neurons based on different shanks in the channel map file.

--- ePhys-Spikes/Spike-Sorting-Code/post_msort_processing/rickshaw_postprocess_full.py
+++ ePhys-Spikes/Spike-Sorting-Code/post_msort_processing/rickshaw_postprocess_full.py
@@ -7,14 +7,17 @@ Created on Sun Nov  6 20:30:52 2022
 """
 
 from discard_noise_and_viz_HR import *
+from population_analysis import *
 import os
 from natsort import natsorted
 # Automate batch processing of the pre processing step
 
 input_dir = '/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sort/spikesort_out/Haad/bc7'
+CHANNEL_MAP_FPATH = '/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sort/channel_maps/128chMap_flex.mat'
 source_dir_list = natsorted(os.listdir(input_dir))
 
 for iter, filename in enumerate(source_dir_list):
     print(iter, ' ', filename )
     Raw_dir = os.path.join(input_dir, filename)
     func_discard_noise_and_viz(Raw_dir)
+    func_pop_analysis(Raw_dir,CHANNEL_MAP_FPATH)
