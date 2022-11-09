1a604971c93b3c5534488b8d0e80d29bfe1541e3
Haad <haad.rathore@gmail.com>
2022-11-08 18:04:58 -0600

    Merged spike sorting script with python script
    Now you only need to call rickshaw_preprocess_HR.py to perform both the preprocessing steps and the spike sorting step. Remember to have mountainlab installed in the conda environment when running this script. Also it is best to run this script from bash directly instead of IPython in Spyder

--- ePhys-Spikes/Spike-Sorting-Code/preprocess_rhd/rickshaw_preprocess_HR.py
+++ ePhys-Spikes/Spike-Sorting-Code/preprocess_rhd/rickshaw_preprocess_HR.py
@@ -5,19 +5,45 @@ Created on Sun Nov  6 20:30:52 2022
 
 @author: luanlab
 """
-
-from preprocess_rhd_HR import *
-import os
-from natsort import natsorted
 # Automate batch processing of the pre processing step
+from preprocess_rhd_HR import *
+import os, subprocess, json
+from natsort import natsorted
 
-input_dir = '/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sort/data/HR/bc7'
-output_dir = '/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sort/spikesort_out/Haad/bc7/'
+# input_dir
+#         |
+#         |__11-02/
+#         |__11-03/
+#         |__11-04/
+# Each session folder must have all the .rhd files and the whisker_stim.txt file
+
+# Folder location inputs
+input_dir = '/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sort/data/HR/temp_RY'
+output_dir = '/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sort/spikesort_out/Haad/temp_RY/'
 CHANNEL_MAP_FPATH = "/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sort/channel_maps/128chMap_flex.mat"
-ELECTRODE_2X16 = False
-source_dir_list = natsorted(os.listdir(input_dir))
 
+# Mountain Sort inputs
+ELECTRODE_2X16 = False
+num_features_var = "8"
+max_num_clips_for_pca_var = "1000"
+
+source_dir_list = natsorted(os.listdir(input_dir))
 for iter, filename in enumerate(source_dir_list):
     print(iter, ' ', filename )
     Raw_dir = os.path.join(input_dir, filename)
+    output_dir_MS = os.path.join(output_dir, filename)
+
+    # Running preprocessing step
     func_preprocess(Raw_dir, output_dir, ELECTRODE_2X16, CHANNEL_MAP_FPATH)
+
+    # Preparing for MountainSort script
+    # Reading from .json file
+    file_pre_ms = os.path.join(output_dir_MS,'pre_MS.json')
+    with open(file_pre_ms, 'r') as f:
+      data_pre_ms = json.load(f)
+    F_SAMPLE = str(data_pre_ms['SampleRate'])
+    # geom.csv 
+    geom_filepath = os.path.join(output_dir_MS,'geom.csv')
+    
+    # Calling MountainSort Bash script
+    subprocess.call(['bash','./mountainSort128_stroke_hyr2.sh',output_dir_MS,output_dir_MS,F_SAMPLE,geom_filepath,num_features_var,max_num_clips_for_pca_var])
