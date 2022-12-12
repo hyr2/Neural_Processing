a275f01732f22a8511ec564a23f428be2f7fd2a3
Haad <haad.rathore@gmail.com>
2022-12-12 13:10:42 -0600

    Automatic deletion of filt.mda and converted_data.mda files
    Once curation is done, these files are no longer needed.

--- ePhys-Spikes/Spike-Sorting-Code/post_msort_processing/rickshaw_postprocess_full.py
+++ ePhys-Spikes/Spike-Sorting-Code/post_msort_processing/rickshaw_postprocess_full.py
@@ -12,12 +12,17 @@ import os
 from natsort import natsorted
 # Automate batch processing of the pre processing step
 
-input_dir = '/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sort/spikesort_out/Haad/temp_RY'
+input_dir = '/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sort/spikesort_out/Haad/BC8'
 CHANNEL_MAP_FPATH = '/media/luanlab/Data_Processing/Jim-Zhang/Spike-Sort/channel_maps/128chMap_flex.mat'
 source_dir_list = natsorted(os.listdir(input_dir))
 
 for iter, filename in enumerate(source_dir_list):
     print(iter, ' ', filename )
     Raw_dir = os.path.join(input_dir, filename)
-    func_discard_noise_and_viz(Raw_dir)
+    # func_discard_noise_and_viz(Raw_dir)
     # func_pop_analysis(Raw_dir,CHANNEL_MAP_FPATH)
+    # delete converted_data.mda and filt.mda
+    os.remove(os.path.join(Raw_dir,'converted_data.mda'))
+    os.remove(os.path.join(Raw_dir,'filt.mda'))
+
+
