b179985a4ae83a126100da5582c2c35e71081b9c
jiaaoz <jz103@rice.edu>
2023-01-24 22:01:36 -0600

    Started

---
+++ correlation_bw_modalities/correlation.py
@@ -0,0 +1,6 @@
+# TODO
+# (1) read source time series and their meta-information
+# (2) split into trials and align time series for each trial.
+# (3) make sure the two timeseries have the same sampling rate by interpolation or downsampling
+# (4) Calculate cross correlation for each trial and find peak time & normalized peak value.
+# (5) Plot demo for each unit; plot peaks over longitudinal.
