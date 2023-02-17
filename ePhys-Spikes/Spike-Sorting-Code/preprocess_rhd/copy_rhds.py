c49d1f0884eefee2fec9aea2517ab1a4621b04b8
jiaaoz <jz103@rice.edu>
2023-02-17 17:53:30 -0600

    COPY_RHD: warn or raise if whiskerstim nonexistant

--- ePhys-Spikes/Spike-Sorting-Code/preprocess_rhd/copy_rhds.py
+++ ePhys-Spikes/Spike-Sorting-Code/preprocess_rhd/copy_rhds.py
@@ -1,30 +1,52 @@
 import shutil
 import os
-​
-rawfolder = "/media/G1/xl_neurovascular_coupling/November_2021 - NVC/BC6/"
-locfolder = "/media/hanlin/Liuyang_10T_backup/jiaaoZ/128ch/spikeSorting128chHaad/data/BC6"
+import glob
+import warnings
+
+rawfolder = "/media/G1/xl_neurovascular_coupling/November_2021 - NVC/RH-9/"
+locfolder = "/media/hanlin/Liuyang_10T_backup/jiaaoZ/128ch/spikeSorting128chHaad/data/RH-9"
 if not os.path.exists(locfolder):
     os.makedirs(locfolder)
-​
+
+
 sessions = [
-    #"11-11-2021", "11-23-2021",
-    #"12-30-2022", "11-18-2021",  "11-30-2021", 
-    "12-07-2021", # this date is invalid: main whisker gone.
-    # "11-2-2021", 
-    # "11-4-2021", 
-    # "12-14-2021"
+    "22-12-14",
+    "22-12-15",
+    "22-12-16",
+    "22-12-17"
+    # "22-12-19",
+    # "22-12-23",
+    # "22-12-28",
+    # "23-01-04",
+    # "23-01-11",
+    # "23-01-18",
+    # "23-01-25",
+    # "23-02-01",
+    # "23-02-08",
 ]
-​
+
+
 for session in sessions:
-    rawpath = os.path.join(rawfolder, session, "ePhys", "NVC", "data_211207_152726")
-    stimtxtpath = os.path.join(rawpath, "whisker_stim.txt")
+    print(session)
+    ephys_path_pattern = os.path.join(rawfolder, session, "ePhys", "**/*.rhd")
+    ephys_rhd_paths = glob.glob(ephys_path_pattern, recursive=True)
+
+    stimtxt_path_pattern = os.path.join(rawfolder, session, "**/whisker_stim.txt")
+    stimtxt_paths = glob.glob(stimtxt_path_pattern, recursive=True)
+    if len(stimtxt_paths)>0:
+        stimtxt_path = stimtxt_paths[0]
+        print ("    %s"%stimtxt_path)
+    else:
+        warnings.warn("Session %s has no `whisker_stim.txt`!"%(session))
+        # raise FileNotFoundError("Session %s has no `whisker_stim.txt`!"%(session))
+
     locpath = os.path.join(locfolder, session)
     if not os.path.exists(locpath):
         os.makedirs(locpath)
-    rhdfiles = list(filter(lambda x: x.endswith(".rhd"), os.listdir(rawpath)))
-    print(rawpath)
-    for rhdfile in rhdfiles:
-        print("    Copying %s to %s" % (os.path.join(rawpath, rhdfile), locpath))
-        shutil.copy(os.path.join(rawpath, rhdfile), locpath)
-    print("    Copying %s to %s" % (stimtxtpath, locpath))
-    shutil.copy(stimtxtpath, locpath)
+    
+    for rhdfile in ephys_rhd_paths:
+        print("    Copying %s to %s" % (rhdfile, locpath))
+        shutil.copy(rhdfile, locpath)
+    if len(stimtxt_paths)>0:
+        print("    Copying %s to %s" % (stimtxt_path, locpath))
+        shutil.copy(stimtxt_path, locpath)
