d60194ba290ae11a0c31bbd7b0172b9c5deb7774
jiaaoz <jz103@rice.edu>
2023-02-28 10:50:59 -0600

    Removes extra interpolation when getting the resampled time stamp

--- ePhys-Spikes/Spike-Sorting-Code/post_msort_processing/utils/waveform_metrics.py
+++ ePhys-Spikes/Spike-Sorting-Code/post_msort_processing/utils/waveform_metrics.py
@@ -24,9 +24,9 @@ def calc_t2p(waveform, fs, target_fs=100000, n_clipout=0):
     fs_over = oversampling * fs
     oversample_interval = np.mean(np.diff(time_waveform_raw))/oversampling
     oversample_nsamples = int(np.floor((time_waveform_raw[-1] - time_waveform_raw[0])/oversample_interval))
-    tw_evaluatepoints = np.arange(oversample_nsamples)*oversample_interval + time_waveform_raw[0]
-    time_interpolator = interp1d(time_waveform_raw, time_waveform_raw, kind='cubic', assume_sorted=True)
-    time_waveform = time_interpolator(tw_evaluatepoints)
+    time_waveform = np.arange(oversample_nsamples)*oversample_interval + time_waveform_raw[0]
+    # time_interpolator = interp1d(time_waveform_raw, time_waveform_raw, kind='cubic', assume_sorted=True)
+    # time_waveform = time_interpolator(tw_evaluatepoints)
     # return time_waveform, fs_over
     trough_mask = np.logical_and(time_waveform>=-0.25, time_waveform<=0.25)
     trough_interval = np.where(trough_mask)[0][[0,-1]]
