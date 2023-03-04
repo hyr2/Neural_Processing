6b113d3de501fcd2319f8817d94ee79cfda3c881
jiaaoz <jz103@rice.edu>
2023-03-04 13:57:43 -0600

    calculate CSD and other modifications

--- correlation_bw_modalities/utils_cc/__init__.py
+++ correlation_bw_modalities/utils_cc/__init__.py
@@ -2,10 +2,12 @@ import json
 import os
 import contextlib
 import sys
+import itertools
 
 from .utils_crossmod import *
 from .read_mda import readmda
 from .read_stimtxt import read_stimtxt
+from .csd import inverse_csd
 
 @contextlib.contextmanager
 def logtofile(filename="/dev/null"):
@@ -15,3 +17,7 @@ def logtofile(filename="/dev/null"):
     yield
     f_stdout.close()
     sys.stdout = oldstdout
+
+def sort_and_groupby(iterable, *, keyfunc):
+    for (kname, elems) in itertools.groupby(sorted(iterable, key=keyfunc), key=keyfunc):
+        yield kname, list(elems)
