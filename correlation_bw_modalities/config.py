763d2136bd11c611663b5da58afbc10835d9ebc1
jiaaoZ <jz103@rice.edu>
2023-04-04 20:30:26 -0500

    update config

---
+++ correlation_bw_modalities/config.py
@@ -0,0 +1,55 @@
+rhd_rawdir = "/media/hanlin/Liuyang_10T_backup/jiaaoZ/128ch/spikeSorting128chHaad/data/"
+npz_rawdir = "/media/hanlin/Liuyang_10T_backup/jiaaoZ/mytempfolder/"
+spk_inpdir = "/media/hanlin/Liuyang_10T_backup/jiaaoZ/128ch/spikeSorting128chHaad/spikesort_out"
+lfp_resdir = "/media/hanlin/Liuyang_10T_backup/jiaaoZ/128ch/spikeSorting128chHaad/csd_ana_res"
+lfp_reldirs = [ # TODO RH8 BC7 as well
+    # "RH-9/22-12-14",
+    # "RH-9/22-12-15",
+    # "RH-9/22-12-16",
+    # "RH-9/23-02-01",
+    # "RH-9/23-02-08",
+    #
+    # "BC8/2021-11-18",
+    # "BC8/2021-11-19",
+    # "BC8/2021-11-24",
+    # "BC8/2021-12-19",
+    # "BC8/2022-01-09",
+    #
+    # "BC7/2021-12-06",
+    # "BC7/2021-12-07",
+    # "BC7/2021-12-09",
+    # "BC7/2022-01-07",
+    # "BC7/2022-01-21",
+    # 
+    # "RH-8/2022-12-01",
+    # "RH-8/2022-12-03",
+    # "RH-8/2022-12-05",
+    # "RH-8/2023-01-24",
+    "RH-8/2023-01-17",
+]
+spk_reldirs = [
+    # "processed_data_rh9/22-12-14",
+    # "processed_data_rh9/22-12-15",
+    # "processed_data_rh9/22-12-16",
+    # "processed_data_rh9/23-02-01",
+    # "processed_data_rh9/23-02-08",
+    #
+    # "processed_data_bc8_new/2021-11-18",
+    # "processed_data_bc8_new/2021-11-19",
+    # "processed_data_bc8_new/2021-11-24",
+    # "processed_data_bc8_new/2021-12-19",
+    # "processed_data_bc8_new/2022-01-09",
+    #
+    # "processed_data_bc7/2021-12-06",
+    # "processed_data_bc7/2021-12-07",
+    # "processed_data_bc7/2021-12-09",
+    # "processed_data_bc7/2022-01-07",
+    # "processed_data_bc7/2022-01-21",
+    # 
+    # "processed_data_rh8/2022-12-01",
+    # "processed_data_rh8/2022-12-03",
+    # "processed_data_rh8/2022-12-05",
+    # "processed_data_rh8/2023-01-24",
+    "processed_data_rh8/2023-01-17",
+]
+
