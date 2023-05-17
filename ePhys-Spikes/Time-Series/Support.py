f661ac5352f72004958b4dc359bf56ec557c7275
Haad <haad.rathore@gmail.com>
2023-05-17 10:29:28 -0500

    two baselines

--- ePhys-Spikes/Time-Series/Support.py
+++ ePhys-Spikes/Time-Series/Support.py
@@ -283,14 +283,14 @@ def interp_session_loss(data_in, day_local_axis, day_axis_ideal):
     local_indx = np.squeeze(np.where(day_local_axis >= 28 ))
     avg_local = np.rint(np.nanmean(np.reshape(data_in[local_indx,:],[local_indx.size,YY_dim]),axis = 0))
     tmp_indx = np.isin(day_axis_ideal,day_local_axis[local_indx])
-    tmp_indx = ~tmp_indx[5:]    # day 28 is a hard coded (requested by Dr.Lan)
+    tmp_indx = ~tmp_indx[6:]    # day 28 is a hard coded (requested by Dr.Lan)
     out_arr_28 = np.zeros([tmp_indx.size,YY_dim])
     out_arr_28[tmp_indx,:] = avg_local
     
     # merging two
     merged_arr = np.vstack((out_arr,out_arr_28))
     local_indx = ~np.isin(day_axis_ideal,day_local_axis)
-    local_indx[:5] = True
+    local_indx[:6] = True
     local_indx = ~local_indx
     
     local_indx_tmp = np.squeeze(np.where(day_local_axis >= 28 ))
@@ -484,7 +484,7 @@ def append_df_to_excel(filename, df, sheet_name='Sheet1', startrow=None,
     
 def bsl_norm(time_series):
     # 1D timeseries (baeline is the first data point by default)
-    bsl_mean = time_series[0]
+    bsl_mean = (time_series[0] + time_series[1]) / 2
     # if bsl_mean == 0:
     #     time_series_out = np.nan * np.ones(time_series.shape) 
     # else:
