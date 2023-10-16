6a03ade3f7673dc5086bd4bceb6e5559e70813fa
Haad <haad.rathore@gmail.com>
2023-10-15 19:10:08 -0500

    bash output during sorting is now saved in a *.log file

--- ePhys-Spikes/Spike-Sorting-Code/preprocess_rhd/mountainSort128_stroke_hyr2.sh
+++ ePhys-Spikes/Spike-Sorting-Code/preprocess_rhd/mountainSort128_stroke_hyr2.sh
@@ -6,75 +6,85 @@ num_features_var=$5
 max_num_clips_for_pca_var=$6
 ml_temp_dir=$7
 
+filename_log=$(date +%F)
+parentdir="$(dirname "$ouput_dir")"
+DEBUGLOG="$parentdir/$filename_log.log"
+echo "Log file path: $DEBUGLOG"
+touch "$DEBUGLOG"
 
 export ML_TEMPORARY_DIRECTORY=$ml_temp_dir
 
-# Run bandpass filter stage of Mountainsort
-ml-run-process ephys.bandpass_filter \
-	--inputs timeseries:$input_dir/converted_data.mda \
-	--outputs timeseries_out:$ouput_dir/filt.mda \
-	--parameters samplerate:$samplerate freq_min:250 freq_max:5000
+{
 
-ml-run-process ephys.whiten \
-	--inputs timeseries:$ouput_dir/filt.mda \
-	--outputs timeseries_out:$ouput_dir/pre1.mda.prv
+		# Run bandpass filter stage of Mountainsort
+		ml-run-process ephys.bandpass_filter \
+			--inputs timeseries:$input_dir/converted_data.mda \
+			--outputs timeseries_out:$ouput_dir/filt.mda \
+			--parameters samplerate:$samplerate freq_min:250 freq_max:5000
 
-ml-run-process ephys.mask_out_artifacts \
---inputs timeseries:$ouput_dir/pre1.mda.prv \ --outputs timeseries_out:$ouput_dir/pre.mda.prv
+		ml-run-process ephys.whiten \
+			--inputs timeseries:$ouput_dir/filt.mda \
+			--outputs timeseries_out:$ouput_dir/pre1.mda.prv
 
-# Spike sorting
-# Specify the detect threshold in standard deviations
-ml-run-process ms4alg.sort \
-	--inputs \
-		timeseries:$ouput_dir/pre.mda.prv \
-		geom:$geom_file \
-	--outputs \
-		firings_out:$ouput_dir/firings.mda \
-	--parameters \
-		detect_sign:0 \
-		adjacency_radius:100 \
-		detect_threshold:4.5 \
-		clip_size:100 \
-		num_features:$num_features_var \
-		max_num_clips_for_pca:$max_num_clips_for_pca_var \
-		
-# Compute cluster metrics
-ml-run-process ephys.compute_cluster_metrics \
-	--inputs \
-		timeseries:$ouput_dir/pre.mda.prv firings:$ouput_dir/firings.mda \
-	--outputs \
-		metrics_out:$ouput_dir/cluster_metrics.json \
-	--parameters \
-		samplerate:$samplerate \
-		clip_size:100 \
-		refrac_msec:2 \
+		ml-run-process ephys.mask_out_artifacts \
+		--inputs timeseries:$ouput_dir/pre1.mda.prv \ --outputs timeseries_out:$ouput_dir/pre.mda.prv
 
-# Compute templates from just filtered data (preferred since we get amplitudes in uV)
-ml-run-process ephys.compute_templates \
-	--inputs \
-		timeseries:$ouput_dir/filt.mda firings:$ouput_dir/firings.mda \
-	--outputs \
-		templates_out:$ouput_dir/templates.mda \
-	--parameters \
-		clip_size:100
+		# Spike sorting
+		# Specify the detect threshold in standard deviations
+		ml-run-process ms4alg.sort \
+			--inputs \
+				timeseries:$ouput_dir/pre.mda.prv \
+				geom:$geom_file \
+			--outputs \
+				firings_out:$ouput_dir/firings.mda \
+			--parameters \
+				detect_sign:0 \
+				adjacency_radius:100 \
+				detect_threshold:4.5 \
+				clip_size:100 \
+				num_features:$num_features_var \
+				max_num_clips_for_pca:$max_num_clips_for_pca_var \
+				
+		# Compute cluster metrics
+		ml-run-process ephys.compute_cluster_metrics \
+			--inputs \
+				timeseries:$ouput_dir/pre.mda.prv firings:$ouput_dir/firings.mda \
+			--outputs \
+				metrics_out:$ouput_dir/cluster_metrics.json \
+			--parameters \
+				samplerate:$samplerate \
+				clip_size:100 \
+				refrac_msec:2 \
 
-# Some more cluster metrics
-ml-run-process ms3.isolation_metrics \
-	--inputs \
-		timeseries:$ouput_dir/pre.mda.prv firings:$ouput_dir/firings.mda \
-	--outputs \
-		metrics_out:$ouput_dir/isolation_metrics_out.json \
-		pair_metrics_out:$ouput_dir/pair_metrics_out.json \
-	--parameters \
-		compute_bursting_parents:true
+		# Compute templates from just filtered data (preferred since we get amplitudes in uV)
+		ml-run-process ephys.compute_templates \
+			--inputs \
+				timeseries:$ouput_dir/filt.mda firings:$ouput_dir/firings.mda \
+			--outputs \
+				templates_out:$ouput_dir/templates.mda \
+			--parameters \
+				clip_size:100
 
-# combine metrics
-ml-run-process ms3.combine_cluster_metrics \
-	--inputs \
-		metrics_list:$ouput_dir/cluster_metrics.json \
-		metrics_list:$ouput_dir/isolation_metrics_out.json \
-	--outputs \
-		metrics_out:$ouput_dir/combine_metrics_new.json \
+		# Some more cluster metrics
+		ml-run-process ms3.isolation_metrics \
+			--inputs \
+				timeseries:$ouput_dir/pre.mda.prv firings:$ouput_dir/firings.mda \
+			--outputs \
+				metrics_out:$ouput_dir/isolation_metrics_out.json \
+				pair_metrics_out:$ouput_dir/pair_metrics_out.json \
+			--parameters \
+				compute_bursting_parents:true
+
+		# combine metrics
+		ml-run-process ms3.combine_cluster_metrics \
+			--inputs \
+				metrics_list:$ouput_dir/cluster_metrics.json \
+				metrics_list:$ouput_dir/isolation_metrics_out.json \
+			--outputs \
+				metrics_out:$ouput_dir/combine_metrics_new.json \
+
+} 2>&1 | tee -a $DEBUGLOG
+
 
 # Mountain View
 # You can do manual curation of clusters using Mountain View
