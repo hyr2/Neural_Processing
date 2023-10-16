d3101a86802cb92188ccf912a4936fbaad59bdf4
Haad <haad.rathore@gmail.com>
2023-10-15 19:07:00 -0500

    Added cortical area of activation plots and change in maps (statistical)

--- neuroimaging_IOS/IOS_Processing/Combine_animals.m
+++ neuroimaging_IOS/IOS_Processing/Combine_animals.m
@@ -9,6 +9,7 @@ mouse_rh9 = '/home/hyr2-office/Documents/Data/IOS_imaging/rh9/';
 mouse_rh11 = '/home/hyr2-office/Documents/Data/IOS_imaging/rh11/';
 % mouse_bbc5 = '/home/hyr2-office/Documents/Data/IOS_imaging/bbc5/';
 
+num_animals = 6;    % number of animals
 output_dir = '/home/hyr2-office/Documents/Data/IOS_imaging/';
 
 % BC7
@@ -16,6 +17,7 @@ file_X = dir_sorted(mouse_bc7);
 file_X = {file_X.name};
 file_X =  file_X(~ismember(file_X,{'.','..'}));
 struct_bc7 = {};
+struct_bc7_580 = {};
 iter_local = 0; 
 xaxis_bc7 = [-5,-3,2,7,14,21,28,42];  %(baselines: 10-24,10-27)
 for iter_filename = file_X
@@ -26,6 +28,8 @@ for iter_filename = file_X
     if isdir(source_dir)
         File_load_local = fullfile(source_dir,'Processed','mat_files','480nm_processed.mat');
         struct_bc7{iter_local} = load(File_load_local);
+        File_load_local = fullfile(source_dir,'Processed','mat_files','580nm_processed.mat');
+        struct_bc7_580{iter_local} = load(File_load_local);
     end
 end
 % RH3
@@ -33,6 +37,7 @@ file_X = dir_sorted(mouse_rh3);
 file_X = {file_X.name};
 file_X =  file_X(~ismember(file_X,{'.','..'}));
 struct_rh3 = {};
+struct_rh3_580 = {};
 iter_local = 0; 
 xaxis_rh3 = [-5,-3,2,7,14,21,28];  %(baselines: 10-24,10-27)
 for iter_filename = file_X
@@ -43,6 +48,8 @@ for iter_filename = file_X
     if isdir(source_dir)
         File_load_local = fullfile(source_dir,'Processed','mat_files','480nm_processed.mat');
         struct_rh3{iter_local} = load(File_load_local);
+        File_load_local = fullfile(source_dir,'Processed','mat_files','580nm_processed.mat');
+        struct_rh3_580{iter_local} = load(File_load_local);
     end
 end
 % BBC5
@@ -67,6 +74,7 @@ file_X = dir_sorted(mouse_rh11);
 file_X = {file_X.name};
 file_X =  file_X(~ismember(file_X,{'.','..'}));
 struct_rh11 = {};
+struct_rh11_580 = {};
 iter_local = 0; 
 xaxis_rh11 = [-5,-3,2,7,14,21,28,35,42,49,56];  %(baselines: 10-24,10-27)
 for iter_filename = file_X
@@ -77,6 +85,8 @@ for iter_filename = file_X
     if isdir(source_dir)
         File_load_local = fullfile(source_dir,'Processed','mat_files','480nm_processed.mat');
         struct_rh11{iter_local} = load(File_load_local);
+        File_load_local = fullfile(source_dir,'Processed','mat_files','580nm_processed.mat');
+        struct_rh11_580{iter_local} = load(File_load_local);
     end
 end
 % RH7
@@ -84,6 +94,7 @@ file_X = dir_sorted(mouse_rh7);
 file_X = {file_X.name};
 file_X =  file_X(~ismember(file_X,{'.','..'}));
 struct_rh7 = {};
+struct_rh7_580 = {};
 iter_local = 0; 
 xaxis_rh7 = [-5,-3,2,7,14,21,28,35,42,49,56];  %(baselines: 10-24,10-27)
 for iter_filename = file_X
@@ -94,6 +105,8 @@ for iter_filename = file_X
     if isdir(source_dir)
         File_load_local = fullfile(source_dir,'Processed','mat_files','480nm_processed.mat');
         struct_rh7{iter_local} = load(File_load_local);
+        File_load_local = fullfile(source_dir,'Processed','mat_files','580nm_processed.mat');
+        struct_rh7_580{iter_local} = load(File_load_local);
     end
 end
 % RH8
@@ -101,8 +114,9 @@ file_X = dir_sorted(mouse_rh8);
 file_X = {file_X.name};
 file_X =  file_X(~ismember(file_X,{'.','..'}));
 struct_rh8 = {};
+struct_rh8_580 = {};
 iter_local = 0;
-xaxis_rh8 = [-3,-5,2,7,14,21,28,35,42,49,56];   %(baselines: 12-3,12-5)
+xaxis_rh8 = [-5,-3,2,7,14,21,28,35,42,49,56];   %(baselines: 12-3,12-5)
 for iter_filename = file_X
     iter_local = iter_local + 1;
     iter_filename = string(iter_filename);
@@ -111,6 +125,8 @@ for iter_filename = file_X
     if isdir(source_dir)
         File_load_local = fullfile(source_dir,'Processed','mat_files','480nm_processed.mat');
         struct_rh8{iter_local} = load(File_load_local);
+        File_load_local = fullfile(source_dir,'Processed','mat_files','580nm_processed.mat');
+        struct_rh8_580{iter_local} = load(File_load_local);
     end
 end
 % RH9
@@ -118,8 +134,9 @@ file_X = dir_sorted(mouse_rh9);
 file_X = {file_X.name};
 file_X =  file_X(~ismember(file_X,{'.','..'}));
 struct_rh9 = {};
+struct_rh9_580 = {};
 iter_local = 0; 
-xaxis_rh9 = [-5,-3,2,7,14,21,28,35,42]; %(baselines: 12-16,12-17)
+xaxis_rh9 = [-5,-3,2,7,14,21,28,35,42,49]; %(baselines: 12-16,12-17)
 for iter_filename = file_X
     iter_local = iter_local + 1;
     iter_filename = string(iter_filename);
@@ -128,21 +145,23 @@ for iter_filename = file_X
     if isdir(source_dir)
         File_load_local = fullfile(source_dir,'Processed','mat_files','480nm_processed.mat');
         struct_rh9{iter_local} = load(File_load_local);
+        File_load_local = fullfile(source_dir,'Processed','mat_files','580nm_processed.mat');
+        struct_rh9_580{iter_local} = load(File_load_local);
     end
 end
 bsl_indx = [1,2];
 filename_fig = fullfile(output_dir,'bc7_out');
-out_bc7 = func_plot3d(struct_bc7,xaxis_bc7,filename_fig,bsl_indx);
+out_bc7 = func_plot3d(struct_bc7,struct_bc7_580,xaxis_bc7,filename_fig,bsl_indx);
 filename_fig = fullfile(output_dir,'rh3_out');
-out_rh3 = func_plot3d(struct_rh3,xaxis_rh3,filename_fig,bsl_indx);
+out_rh3 = func_plot3d(struct_rh3,struct_rh3_580,xaxis_rh3,filename_fig,bsl_indx);
 filename_fig = fullfile(output_dir,'rh7_out');
-out_rh7 = func_plot3d(struct_rh7,xaxis_rh7,filename_fig,bsl_indx);
+out_rh7 = func_plot3d(struct_rh7,struct_rh7_580,xaxis_rh7,filename_fig,bsl_indx);
 filename_fig = fullfile(output_dir,'rh8_out');
-out_rh8 = func_plot3d(struct_rh8,xaxis_rh8,filename_fig,bsl_indx);
+out_rh8 = func_plot3d(struct_rh8,struct_rh8_580,xaxis_rh8,filename_fig,bsl_indx);
 filename_fig = fullfile(output_dir,'rh9_out');
-out_rh9 = func_plot3d(struct_rh9,xaxis_rh9,filename_fig,bsl_indx);
+out_rh9 = func_plot3d(struct_rh9,struct_rh9_580,xaxis_rh9,filename_fig,bsl_indx);
 filename_fig = fullfile(output_dir,'rh11_out');
-out_rh11 = func_plot3d(struct_rh11,xaxis_rh11,filename_fig,bsl_indx);
+out_rh11 = func_plot3d(struct_rh11,struct_rh11_580,xaxis_rh11,filename_fig,bsl_indx);
 % filename_fig = fullfile(output_dir,'bbc5_out');
 % out_bbc5 = func_plot3d(struct_bbc5,xaxis_bbc5,filename_fig);
 
@@ -161,12 +180,65 @@ area_vec = [out_rh3{1},out_rh7{1},out_rh8{1},out_rh9{1},out_rh11{1}];
 avg_area = mean(area_vec);
 sem_area = std(area_vec)/sqrt(length(area_vec));
 
+% Statistics on average distance shifts post stroke
+shift_vec_early = [out_rh3{5}(find(out_rh3{4} < 21))',out_rh7{5}(find(out_rh7{4} < 21))',out_rh8{5}(find(out_rh8{4} < 21))',out_rh9{5}(find(out_rh9{4} < 21))',out_rh11{5}(find(out_rh11{4} < 21))'];
+shift_vec_chronic = [out_rh3{5}(find(out_rh3{4} > 21))',out_rh7{5}(find(out_rh7{4} > 21))',out_rh8{5}(find(out_rh8{4} > 21))',out_rh9{5}(find(out_rh9{4} > 21))',out_rh11{5}(find(out_rh11{4} > 21))'];
+
+avg_shift_early = mean(shift_vec_early);
+avg_shift_chronic = mean(shift_vec_chronic);
+sem_shift_early = std(shift_vec_early)/sqrt(length(shift_vec_early));
+sem_shift_chronic = std(shift_vec_chronic)/sqrt(length(shift_vec_chronic));
+
+
 string_out = sprintf('Avg shift in X : %.3f +/- %.3f mm', avg_X, sem_X);
 disp(string_out);
 string_out = sprintf('Avg shift in Y : %.3f +/- %.3f mm', avg_Y, sem_Y);
 disp(string_out);
 string_out = sprintf('Avg ratio of area : %.3f +/- %.3f', avg_area, sem_area);
 disp(string_out);
+string_out = sprintf('Avg shift of cortical activation map (early) : %.3f +/- %.3f mm', avg_shift_early, sem_shift_early);
+disp(string_out);
+string_out = sprintf('Avg shift of cortical activation map (chronic) : %.3f +/- %.3f mm', avg_shift_chronic, sem_shift_chronic);
+disp(string_out);
+
+%% Statistical tests
+[h,p] = ttest2(shift_vec_early,shift_vec_chronic);
+string_out = sprintf('The shift in cortical activity map has a p-value of: %.4f', p);
+disp(string_out);
+
+%% Plots for Figure1G (inter-day shift):
+histogram(shift_vec_early, 'BinWidth', 0.312,'FaceColor', [0.1, 0.9, 0.9],'EdgeColor',[0.08, 0.65, 0.9]);
+hold on;
+histogram(-shift_vec_chronic, 'BinWidth', 0.312,'FaceColor', [0.9, 0.1, 0.2],'EdgeColor',[1.0, 0.05, 0.1]);
+xlim([-2,2.1])
+xlabel('Inter-day Shift (mm)');
+ylabel('Count');
+
+%% Plots for Figure1G (area):
+xaxis_ideal = [-3, 2, 7, 14,21,28,35,42,49,56];
+
+arr_area_all = zeros(num_animals,size(xaxis_ideal,2));
+arr_area_all(1,:) = out_rh3{6};
+arr_area_all(2,:) = out_rh7{6};
+arr_area_all(3,:) = out_rh8{6};
+arr_area_all(4,:) = out_rh9{6}; % Areas are messed up. Check this
+arr_area_all(5,:) = out_rh11{6};
+arr_area_all(6,:) = out_bc7{6};     % day 7 is messed up
+nanmean(arr_area_all,1)
+
+% out_rh3{6} = helper_fill_nans(xaxis_ideal,xaxis_rh3(2:end),out_rh3{6});
+% out_rh3{6} = helper_fill_nans(xaxis_ideal,xaxis_rh3(2:end),out_rh3{6});
+% out_rh3{6} = helper_fill_nans(xaxis_ideal,xaxis_rh3(2:end),out_rh3{6});
+% out_rh3{6} = helper_fill_nans(xaxis_ideal,xaxis_rh3(2:end),out_rh3{6});
+% out_rh3{6} = helper_fill_nans(xaxis_ideal,xaxis_rh3(2:end),out_rh3{6});
+
+
+histogram(shift_vec_early, 'BinWidth', 0.312,'FaceColor', [0.1, 0.9, 0.9],'EdgeColor',[0.08, 0.65, 0.9]);
+hold on;
+histogram(-shift_vec_chronic, 'BinWidth', 0.312,'FaceColor', [0.9, 0.1, 0.2],'EdgeColor',[1.0, 0.05, 0.1]);
+xlim([-2,2.1])
+xlabel('Inter-day Shift (mm)');
+ylabel('Count');
 
 % Combining X axes for Rh7-9
 % filename_fig = fullfile(pwd,'combined_animals');
@@ -175,6 +247,3 @@ disp(string_out);
 % func_plot2d_multiY(struct_rh7,struct_rh8,struct_rh9,xaxis_rh7,xaxis_rh8,xaxis_rh9,filename_fig);
 
 
-% To add: 
-% 1. average change in centroid due to stroke in X
-% 2. average change in centroid due to stroke in Y
--- neuroimaging_IOS/IOS_Processing/func_plot3d.m
+++ neuroimaging_IOS/IOS_Processing/func_plot3d.m
@@ -1,7 +1,8 @@
-function [out_arr] = func_plot3d(struct_in,xaxis_in,filename_fig,bsl_indx)
+function [out_arr] = func_plot3d(struct_in,struct_in_580,xaxis_in,filename_fig,bsl_indx)
 
 % output array contains the relative area and the relative shift in center
-out_arr = {};
+out_arr = cell(1,6);
+xaxis_ideal = [-3, 2, 7, 14,21,28,35,42,49,56];
 % Plotting the activation area center 
 area = zeros(1,length(struct_in));
 vec_rel = zeros(length(struct_in),2);
@@ -16,19 +17,23 @@ x_axis = 8e-3 * vec_rel(:,1); % in mm
 y_axis = 8e-3 * vec_rel(:,2); % in mm
 x_bsl = (x_axis(1) + x_axis(2))/2;
 x_axis = x_axis - x_bsl;
-area = area/max(area);
+y_bsl = (y_axis(1) + y_axis(2))/2;
+y_axis = y_axis - y_bsl;
+area = area/mean(area(bsl_indx));
 colors = [0,0,1;0,0,1];
 sz_tmp = size(x_axis,1) - size(bsl_indx,2);
 s2 = zeros(sz_tmp,3);
 colors = vertcat(colors,s2);
 
-if sz_tmp > 5
-    out_arr{end+1} = (area(end) + area(end-1))/2;   % avg area 
-    out_arr{end+1} = (x_axis(end) + x_axis(end-1))/2;   % avg x shift
-else
-    out_arr{end+1} = area(end);     % avg area 
-    out_arr{end+1} = x_axis(end);    % avg x shift
-end
+
+ind_z = find(z_axis > 21);
+out_arr{1} = mean(area(ind_z));
+out_arr{2} = mean(x_axis(ind_z));
+
+out_arr{6} = helper_fill_nans(xaxis_ideal,xaxis_in(2:end),area);
+
+% out_arr{6} = area;       % relative area to baseline
+
 z_axis = z_axis(~isnan(y_axis));
 area = area(~isnan(y_axis));
 colors = colors(~isnan(y_axis),:,:);
@@ -46,7 +51,7 @@ zlabel('Days (Post Stroke)');
 grid on;
 lightangle(45, 30); % Adjust lighting angle
 lighting gouraud;   % Smooth shading
-ax = gca;
+% ax = gca;
 ax.XColor = 'k';
 ax.YColor = 'k';
 ax.ZColor = 'k';
@@ -59,22 +64,24 @@ fig.Position = [100, 100, 800, 600];
 
 mkdir(filename_fig)
 view(0, 0); % Change the view angle
-z_axis([3]) = [];   % ignore day 2
-x_axis([3]) = [];   % ignore day 2
-degree = 4;
-coeef = polyfit(z_axis,x_axis,degree);
-z_range = linspace(min(z_axis),max(z_axis),50);
-x_fit = polyval(coeef,z_range);
-smoother_x_fit = smoothdata(x_fit,'loess',10);
+% z_axis([3]) = [];   % ignore day 2
+% x_axis([3]) = [];   % ignore day 2
+% degree = 4;
+% coeef = polyfit(z_axis,x_axis,degree);
+% z_range = linspace(min(z_axis),max(z_axis),50);
+% x_fit = polyval(coeef,z_range);
+% smoother_x_fit = smoothdata(x_fit,'loess',10);
 % Plot the original data and the smoothed polynomial fit
 % figure;
+z_range = z_axis;
 y_val = zeros(1,length(z_range));
 % hold on;
 % plot(gca,x_axis, z_axis, 'o', 'MarkerFaceColor', 'b', 'MarkerEdgeColor', 'b');
 hold on;
-plot3(ax,smoother_x_fit,y_val, z_range, 'Color', '#686869' , 'LineWidth', 2.5);
+plot3(ax,x_axis,y_val, z_range, 'Color', '#686869' , 'LineWidth', 2.5);
+% plot3(ax,smoother_x_fit,y_val, z_range, 'Color', '#686869' , 'LineWidth', 2.5);
 grid off;
-xlim([-1.1,1.1]);
+xlim([-1.4,1.4]);
 zlim([-6,56]);
 xticks([-1,0,1]);
 set(gca,'ZTickLabel',[]);
@@ -89,17 +96,17 @@ x_axis = 8e-3 * vec_rel(:,1); % in mm
 y_axis = 8e-3 * vec_rel(:,2); % in mm
 y_bsl = (y_axis(1) + y_axis(2))/2;
 y_axis = y_axis - y_bsl;
+x_bsl = (x_axis(1) + x_axis(2))/2;
+x_axis = x_axis - x_bsl;
 % area = area/max(area);
 colors = [0,0,1;0,0,1];
 sz_tmp = size(x_axis,1) - size(bsl_indx,2);
 s2 = zeros(sz_tmp,3);
 colors = vertcat(colors,s2);
 
-if sz_tmp > 5
-    out_arr{end+1} = (y_axis(end) + y_axis(end-1))/2;   % avg y shift
-else
-    out_arr{end+1} = y_axis(end);    % avg y shift
-end
+
+out_arr{3} = mean(y_axis(ind_z));
+
 z_axis = z_axis(~isnan(y_axis));
 % area = area(~isnan(y_axis));
 colors = colors(~isnan(y_axis),:,:);
@@ -127,17 +134,19 @@ ax.Layer = 'top';
 fig = gcf;
 fig.Position = [100, 100, 800, 600];
 view(90, 0); % Change the view angle
-y_axis([3]) = [];   % ignore day 2
-z_axis([3]) = [];   % ignore day 2
-coeef = polyfit(z_axis,y_axis,degree);
+% y_axis([3]) = [];   % ignore day 2
+% z_axis([3]) = [];   % ignore day 2
+% coeef = polyfit(z_axis,y_axis,degree);
 % z_range = linspace(min(z_axis),max(z_axis),50);
-y_fit = polyval(coeef,z_range);
-smoother_y_fit = smoothdata(y_fit,'loess',10);
+% y_fit = polyval(coeef,z_range);
+% smoother_y_fit = smoothdata(y_fit,'loess',10);
+z_range = z_axis;
 x_val = zeros(1,length(z_range));
 hold on;
-plot3(ax,x_val,smoother_y_fit, z_range, 'Color', '#686869', 'LineWidth', 2.5);
+plot3(ax,x_val,y_axis, z_range, 'Color', '#686869' , 'LineWidth', 2.5);
+% plot3(ax,x_val,smoother_y_fit, z_range, 'Color', '#686869', 'LineWidth', 2.5);
 grid off;
-ylim([-1.1,1.1]);
+ylim([-1,1]);
 zlim([-6,56]);
 yticks([-1,0,1]);
 set(gca,'ZTickLabel',[]);
@@ -146,5 +155,21 @@ set(ax,'FontSize',24)
 set(fig,'Position',[50 50 400 800])
 saveas(fig,fullfile(filename_fig,'Y.png'),'png');
 close(fig);
+
+%% Distance (shift)
+x_axis([1,2]) = [];
+x_axis = vertcat([0],x_axis);
+y_axis([1,2]) = [];
+y_axis = vertcat([0],y_axis);
+z_axis([1,2]) = [];
+% z_axis = vertcat([-1],z_axis);
+delta_x = diff(x_axis);
+delta_y = diff(y_axis);
+distance_shifts = sqrt(delta_x.^2 + delta_y.^2);
+
+out_arr{4} = z_axis;        % the day axis for this animal corresponds to distancce_shifts only (not the actual day axis)
+out_arr{5} = distance_shifts; % relative shifts between datapoints
+
+
 
 end
