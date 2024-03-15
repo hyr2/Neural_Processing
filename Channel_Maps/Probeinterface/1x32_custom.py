7e4b11d2e76ff21ffdc11f4d5f18cc455beef8d9
Haad <haad.rathore@gmail.com>
2024-03-15 16:06:48 -0500

    Plotting multi-electrode array single shank NET

--- Channel_Maps/Probeinterface/1x32_custom.py
+++ Channel_Maps/Probeinterface/1x32_custom.py
@@ -13,7 +13,7 @@ for i in range(n):
     positions[i] = x, y
 positions *= 25
 
-# Generating Pribe
+# Generating Probe
 probe = Probe(ndim=2, si_units='um')
 probe.set_contacts(positions=positions, shapes='square', shape_params={'width': 20})
 print(probe)
@@ -23,7 +23,7 @@ probe.set_planar_contour(polygon)
 # Plotting
 plot_probe(probe)
 plt.axis('off')
-plt.savefig(os.path.join('/home/hyr2-office/Documents/Paper/SIngle-Figures-SVG-LuanVersion/Fig2/subfigures/','1x32.svg'),format = 'svg',transparent=True)
+# plt.savefig(os.path.join('/home/hyr2-office/Documents/Paper/SIngle-Figures-SVG-LuanVersion/Fig2/subfigures/','1x32.svg'),format = 'svg',transparent=True)
 # 3D
 probe_3d = probe.to_3d(axes='xz')
 probe_3d.rotate(theta=15, center=[0, 0, 0], axis=[1, 0, 0])
@@ -33,5 +33,42 @@ ax = plt.gca()
 ax.view_init(elev=9, azim=104)
 ax.dist = 6
 plt.axis('off')
-plt.savefig(os.path.join('/home/hyr2-office/Documents/Paper/SIngle-Figures-SVG-LuanVersion/Fig2/subfigures/','1x32_3D.svg'),format = 'svg',transparent=True)
+# plt.savefig(os.path.join('/home/hyr2-office/Documents/Paper/SIngle-Figures-SVG-LuanVersion/Fig2/subfigures/','1x32_3D.svg'),format = 'svg',transparent=True)
 plt.show()
+# 2D for Figure2C
+# Generating Probe
+fig = plt.figure()
+ax = fig.add_subplot(1, 1,1)
+probe = Probe(ndim=2, si_units='um')
+probe.set_contacts(positions=positions, shapes='square', shape_params={'width': 20})
+print(probe)
+# contour
+polygon = [(65, -10), (50,-30) ,(35, -10), (-35, 820), (-35, 880), (65, 880)]
+probe.set_planar_contour(polygon)
+# Plotting
+plot_probe(probe,ax=ax, \
+               probe_shape_kwargs={'edgecolor': 'k', 'lw': 0.5, 'alpha': 0.15},contacts_kargs = {'alpha' : 0.3})
+plt.axis('off')
+# plt.savefig(os.path.join('/home/hyr2-office/Documents/Paper/SIngle-Figures-SVG-LuanVersion/Fig2/subfigures/','1x32.svg'),format = 'svg',transparent=True)
+# Generating Probe
+n = 32
+positions = np.zeros((n, 2))
+for i in range(n):
+    x = 2
+    y = i % n
+    positions[i] = x, y
+positions *= 25
+positions[:,1] = positions[:,1] * 2
+fig = plt.figure()
+ax = fig.add_subplot(1, 1,1)
+probe = Probe(ndim=2, si_units='um')
+probe.set_contacts(positions=positions, shapes='square', shape_params={'width': 35})
+print(probe)
+# contour
+polygon = [(85, -10), (50,-50) ,(25, -10), (-65, 820), (-65, 1700), (85, 1700)]
+probe.set_planar_contour(polygon)
+# Plotting
+plot_probe(probe,ax=ax, \
+                probe_shape_kwargs={'edgecolor': 'k', 'lw': 0.5, 'alpha': 0.1},contacts_kargs = {'alpha' : 0.7})
+plt.axis('off')
+plt.savefig(os.path.join('/home/hyr2-office/Documents/Paper/SIngle-Figures-SVG-LuanVersion/Fig2/subfigures/','1x32_Fig2C.svg'),format = 'svg',transparent=True)
